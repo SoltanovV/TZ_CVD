712f4a4601b2f340b126ade87b4b1f67c1f9f50d
Vladislav <vsoltanovv@gmail.com>
2022-09-04 20:59:28 +0200

    Корректировки

--- TZ_CVD/Model/ApplicationContext.cs
+++ TZ_CVD/Model/ApplicationContext.cs
@@ -1,114 +1,114 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TZ_CVD.Model.Entity;
 
-namespace TZ_CVD.Model
+namespace TZ_CVD.Model;
+
+public class ApplicationContext : DbContext
 {
-    public class ApplicationContext : DbContext
+    public DbSet<Employee> Employees { get; set; } = null!;
+    public DbSet<Department> Departments { get; set; } = null!;
+
+    public ApplicationContext() => Database.EnsureCreated();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        public DbSet<Employee> Employees { get; set; } = null!;
-        public DbSet<Department> Departments { get; set; } = null!;
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Department;Trusted_Connection=True;");
+    }
 
-        public ApplicationContext() => Database.EnsureCreated();
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        #region Заполнение данными Department
+        var departament1 = new Department()
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Department;Trusted_Connection=True;");
-        }
+            Id = 1,
+            Name = "D1"
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        };
+
+        var departament2 = new Department()
         {
-            #region Заполнение данными Department
-            var departament1 = new Department()
-            {
-                Id = 1,
-                Name = "D1"
+            Id = 2,
+            Name = "D2",
+        };
 
-            };
+        var departament3 = new Department()
+        {
+            Id = 3,
+            Name = "D3",
+        };
 
-            var departament2 = new Department()
-            {
-                Id = 2,
-                Name = "D2",
-            };
+        var departaments = new List<Department>() { departament1, departament2, departament3 };
+        #endregion
 
-            var departament3 = new Department()
-            {
-                Id = 3,
-                Name = "D3",
-            };
+        #region заполнение данными Employee
+        var employees1 = new Employee()
+        {
+            Id = 1,
+            DepartmentId = 1,
+            ChiefId = 5,
+            Name = "John",
+            Salary = 100
+        };
 
-            var departaments = new List<Department>() { departament1, departament2, departament3 };
-            #endregion
+        var employees2 = new Employee()
+        {
+            Id = 2,
+            DepartmentId = 1,
+            ChiefId = 5,
+            Name = "Misha",
+            Salary = 600
+        };
 
-            #region заполнение данными Employee
-            var employees1 = new Employee()
-            {
-                Id = 1,
-                DepartmentId = 1,
-                ChiefId = 5,
-                Name = "John",
-                Salary = 100
-            };
+        var employees3 = new Employee()
+        {
+            Id = 3,
+            DepartmentId = 2,
+            ChiefId = 6,
+            Name = "Eugen",
+            Salary = 300
+        };
+        var employees4 = new Employee()
+        {
+            Id = 4,
+            DepartmentId = 2,
+            ChiefId = 6,
+            Name = "Tolya",
+            Salary = 400
+        };
+        var employees5 = new Employee()
+        {
+            Id = 5,
+            DepartmentId = 3,
+            ChiefId = 7,
+            Name = "Stepan",
+            Salary = 500
+        };
+        var employees6 = new Employee()
+        {
+            Id = 6,
+            DepartmentId = 3,
+            ChiefId = 7,
+            Name = "Alex",
+            Salary = 1000
+        };
+        var employees7 = new Employee()
+        {
+            Id = 7,
+            DepartmentId = 3,
+            ChiefId = null,
+            Name = "Ivan",
+            Salary = 1100
+        };
+        var employees = new List<Employee>() { employees1, employees2, employees3, employees4, employees5, employees6, employees7 };
+        #endregion
 
-            var employees2 = new Employee()
-            {
-                Id = 2,
-                DepartmentId = 1,
-                ChiefId = 5,
-                Name = "Misha",
-                Salary = 600
-            };
+        modelBuilder.Entity<Department>().HasData(departaments);
+        modelBuilder.Entity<Employee>().HasData(employees);
 
-            var employees3 = new Employee()
-            {
-                Id = 3,
-                DepartmentId = 2,
-                ChiefId = 6,
-                Name = "Eugen",
-                Salary = 300
-            };
-            var employees4 = new Employee()
-            {
-                Id = 4,
-                DepartmentId = 2,
-                ChiefId = 6,
-                Name = "Tolya",
-                Salary = 400
-            };
-            var employees5 = new Employee()
-            {
-                Id = 5,
-                DepartmentId = 3,
-                ChiefId = 7,
-                Name = "Stepan",
-                Salary = 500
-            };
-            var employees6 = new Employee()
-            {
-                Id = 6,
-                DepartmentId = 3,
-                ChiefId = 7,
-                Name = "Alex",
-                Salary = 1000
-            };
-            var employees7 = new Employee()
-            {
-                Id = 7,
-                DepartmentId = 3,
-                ChiefId = null,
-                Name = "Ivan",
-                Salary = 1100
-            };
-            var employees = new List<Employee>() { employees1, employees2, employees3, employees4, employees5, employees6, employees7 };
-            #endregion
+        // Связь один ко многим 
+        modelBuilder.Entity<Employee>()
+            .HasOne(e => e.Department)
+            .WithMany(d => d.Employee);
 
-            modelBuilder.Entity<Department>().HasData(departaments);
-            modelBuilder.Entity<Employee>().HasData(employees);
-
-            // Связь один ко многим 
-            modelBuilder.Entity<Employee>()
-                .HasOne(e => e.Department)
-                .WithMany(d => d.Employee);
-        }
     }
 }
--- TZ_CVD/Program.cs
+++ TZ_CVD/Program.cs
@@ -1,35 +1,38 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using TZ_CVD.Model;
 using TZ_CVD.Model.Entity;
+using TZ_CVD.Model;
 
 using (ApplicationContext db = new ApplicationContext())
 {
     // Полчучение списка всех сотрудников
     var empo = db.Employees.Include(e => e.Department).ToList();
 
-    var s = new string('-', 20);
-
     // Получение списка Id Руководителей
-    var empChiefId = db.Employees.Where(e => e.ChiefId != null).Select(e => e.ChiefId).Distinct().ToList();
+    var chiefId = db.Employees
+        .Where(e => e.ChiefId != null)
+        .Select(e => e.ChiefId)
+        .Distinct()
+        .ToList();
 
-    // Получение руководителей
-    var сhief = viewIdChiefList(empo, empChiefId);
+    List<Employee> chief = new List<Employee>();
+    List<Employee> employee = new List<Employee>();
 
-    // Получение сотрудников
-    var employee = viewIdEmployeeList(empo, empChiefId);
+    viewEmployeeList(empo, chiefId, out chief, out employee);
 
-    Console.WriteLine(s);
-    Console.WriteLine("Суммарную зарплату в разрезе департаментов (без руководителей и с руководителями)");
+    var str = new string('-', 20);
+
+    Console.WriteLine(str);
+    Console.WriteLine("Суммарную зарплату в разрезе департаментов (без руководителей и с руководителями)\n");
     Console.WriteLine("Без руководителей");
 
-    // Группировка по депараменту и выборка
+    // Группировка и выборка по депараменту без руководителей
     var salaryEmp = employee
         .GroupBy(e => e.Department)
         .Select(d => new
         {
             Name = d.Key.Name,
-            Salary = d.Select(e => e.Salary),
+            Salary = d
+            .Select(e => e.Salary),
         });
 
     foreach (var item in salaryEmp)
@@ -37,13 +40,14 @@ using (ApplicationContext db = new ApplicationContext())
         Console.WriteLine($" {item.Name} | {item.Salary.Sum()}  |");
     }
 
-    // Группировка по депараменту и выборка
+    // Группировка и выборка по депараменту с руководителями
     var salaryChief = empo
         .GroupBy(e => e.Department)
         .Select(d => new
         {
             Name = d.Key.Name,
-            Salary = d.Select(e => e.Salary),
+            Salary = d
+            .Select(e => e.Salary),
         });
 
     Console.WriteLine("\nC руководителями");
@@ -53,51 +57,41 @@ using (ApplicationContext db = new ApplicationContext())
     }
 
 
-    Console.WriteLine(s);
+    Console.WriteLine(str);
     Console.WriteLine("Департамент, в котором у сотрудника зарплата максимальна");
 
     // Получение максимальной ЗП
-    var max = employee.Max(e => e.Salary);
+    var max = empo.Max(e => e.Salary);
 
     // Выборка по максимальной Зп
-    var resultMax = db.Employees.Where(e => e.Salary == max).Select(e => e.Department.Name);
+    var resultMax = db.Employees
+        .Where(e => e.Salary == max)
+        .Select(e => new 
+    {
+        Dep = e.Department.Name,
+        Name = e.Name
+    });
+
     foreach (var item in resultMax)
     {
-        Console.WriteLine(item);
+        Console.WriteLine($"{item.Dep} {item.Name}");
     }
 
-    Console.WriteLine(s);
+    Console.WriteLine(str);
     Console.WriteLine("Зарплаты руководителей департаментов (по убыванию)");
-    var res = viewIdList(empo, сhief);
-    res.Reverse();
+    //var result = viewListEmpl(empo, сhief);
+    chief.Reverse();
 
-    foreach (var item in res)
+    foreach (var item in chief)
     {
         Console.WriteLine($"{item.Name} - {item.Salary}");
     }
 
-    // Вспомогательные функции 
-    // Функция для нахождения id руководителей
-    List<Employee> viewIdChiefList (List<Employee> oneList, List<int?> twoList)
+    // Функция для получения руководителей и подчиненых
+    void viewEmployeeList(List<Employee> oneList, List<int?> twoList, out List<Employee> outOneList, out List<Employee> outTwoList)
     {
-        List<Employee> retirnIdList = new List<Employee>();
-        for (int i = 0; i < oneList.Count; i++)
-        {
-            for (int j = 0; j < twoList.Count; j++)
-            {
-                if (oneList[i].Id == twoList[j])
-                {
-                    retirnIdList.Add(oneList[i]);
-                }
-            }
-        }
-        return retirnIdList;
-    }
-
-    // Функция для нахождения id сотрудников
-    List<Employee> viewIdEmployeeList(List<Employee> oneList, List<int?> twoList)
-    {
-        List<Employee> retirnIdList = new List<Employee>();
+        List<Employee> returnChiefList = new List<Employee>();
+        List<Employee> returnEmplList = new List<Employee>();
         int count = 0;
         for (int i = 0; i < oneList.Count; i++)
         {
@@ -106,31 +100,22 @@ using (ApplicationContext db = new ApplicationContext())
                 if (oneList[i].Id != twoList[j])
                 {
                     count++;
+
                     if (count == twoList.Count)
                     {
-                        retirnIdList.Add(oneList[i]);
+                        returnChiefList.Add(oneList[i]);
                     }
+                }
+                else if (oneList[i].Id == twoList[j])
+                {
+                    returnEmplList.Add(oneList[i]);
                 }
             }
             count = 0;
         }
-        return retirnIdList;
-    }
 
-    List<Employee> viewIdList(List<Employee> oneList, List<Employee> twoList)
-    {
-        List<Employee> retirnIdList = new List<Employee>();
-        for (int i = 0; i < oneList.Count; i++)
-        {
-            for (int j = 0; j < twoList.Count; j++)
-            {
-                if (oneList[i].Id == twoList[j].Id)
-                {
-                    retirnIdList.Add(oneList[i]);
-                }
-            }
-        }
-        return retirnIdList;
+        outOneList = returnEmplList;
+        outTwoList = returnChiefList;
     }
 }
 
